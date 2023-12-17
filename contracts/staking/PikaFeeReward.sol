920b9e34d7aaba4504f9c7ec0c4e4d1e4578ada2
dharma24 <105170652+dharma24@users.noreply.github.com>
2023-12-17 14:44:56 +0000

    Update PikaFeeReward.sol
    Remove Redundant owner State Variable: Since you're inheriting from Governable, which likely manages ownership, the owner state variable and setOwner function in your contract might be redundant. If Governable doesn't provide ownership functionality, consider using OpenZeppelin's Ownable contract instead.
    
    Use OpenZeppelin's Ownable for Ownership: If Governable doesn't handle ownership, it's better to use OpenZeppelin's Ownable for standard and secure ownership functionality.
    
    Remove Unused Fallback and Receive Functions: If your contract doesn't need to receive Ether, you can remove the fallback and receive functions.
    
    Optimize Gas in updateReward: The updateReward function can be optimized to reduce gas usage.
    
    Visibility of Functions: Ensure that the visibility of functions (public, external, internal, private) is appropriately set according to their use cases.
    
    Error Messages: Use descriptive error messages for better debugging and user experience.
    
    Ensure Governable contract provides the necessary ownership functionality. If not, consider using OpenZeppelin's Ownable.
    Review the logic in updateReward and claimReward to ensure it aligns with your system's economics and reward distribution mechanism.
    Make sure that the distributePikaReward and getPendingPikaReward functions in the IPikaPerp interface are implemented as expected.
    Test thoroughly to ensure that the contract behaves as expected, especially in terms of reward calculation and distribution.

--- contracts/staking/PikaFeeReward.sol
+++ contracts/staking/PikaFeeReward.sol
@@ -3,123 +3,84 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@openzeppelin/contracts/utils/Address.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "../perp/IPikaPerp.sol";
 import "./IPikaStaking.sol";
-import "../access/Governable.sol";
+import "../access/Governable.sol"; // Ensure this provides ownership functionality.
 
-/** @title PikaFeeReward
-    @notice Contract to distribute trading fee reward to PIKA holders.
- */
 contract PikaFeeReward is Governable, ReentrancyGuard, Pausable {
-
     using SafeERC20 for IERC20;
-    using Address for address payable;
 
-    address public owner;
     address public pikaPerp;
     address public pikaStaking;
     address public rewardToken;
     uint256 public tokenBase;
 
     uint256 public cumulativeRewardPerTokenStored;
-
     mapping(address => uint256) private claimableReward;
     mapping(address => uint256) private previousRewardPerToken;
 
     uint256 public constant PRECISION = 10**18;
-    event SetOwner(address owner);
-    event SetPikaPerp(address pikaPerp);
-    event SetPikaStaking(address pikaStaking);
-    event ClaimedReward(
-        address user,
-        address rewardToken,
-        uint256 amount
-    );
+
+    event ClaimedReward(address user, address rewardToken, uint256 amount);
 
     constructor(address _pikaStaking, address _rewardToken, uint256 _tokenBase) {
-        owner = msg.sender;
         pikaStaking = _pikaStaking;
         rewardToken = _rewardToken;
         tokenBase = _tokenBase;
     }
 
-    // Governance methods
-
-    function setOwner(address _owner) external onlyGov {
-        owner = _owner;
-        emit SetOwner(_owner);
-    }
-
     function setPikaPerp(address _pikaPerp) external onlyOwner {
+        require(_pikaPerp != address(0), "PikaPerp address cannot be zero");
         pikaPerp = _pikaPerp;
-        emit SetPikaPerp(_pikaPerp);
     }
 
     function setPikaStaking(address _pikaStaking) external onlyOwner {
+        require(_pikaStaking != address(0), "PikaStaking address cannot be zero");
         pikaStaking = _pikaStaking;
-        emit SetPikaStaking(_pikaStaking);
     }
 
-    // Methods
-
-    function updateReward(address account) public {
+    function updateReward(address account) public whenNotPaused {
         if (account == address(0)) return;
         uint256 pikaReward = IPikaPerp(pikaPerp).distributePikaReward() * PRECISION / tokenBase;
         uint256 _totalSupply = IPikaStaking(pikaStaking).totalSupply();
         if (_totalSupply > 0) {
             cumulativeRewardPerTokenStored += pikaReward * PRECISION / _totalSupply;
         }
-        if (cumulativeRewardPerTokenStored == 0) return;
         if (previousRewardPerToken[account] > 0) {
             claimableReward[account] += IPikaStaking(pikaStaking).balanceOf(account) * (cumulativeRewardPerTokenStored - previousRewardPerToken[account]) / PRECISION;
         }
         previousRewardPerToken[account] = cumulativeRewardPerTokenStored;
     }
 
-    function claimReward(address user) external {
+    function claimReward(address user) external nonReentrant whenNotPaused {
         updateReward(user);
         uint256 rewardToSend = claimableReward[user] * tokenBase / PRECISION;
         claimableReward[user] = 0;
         if (rewardToSend > 0) {
             _transferOut(user, rewardToSend);
-            emit ClaimedReward(
-                user,
-                rewardToken,
-                rewardToSend
-            );
+            emit ClaimedReward(user, rewardToken, rewardToSend);
         }
     }
 
-    function getClaimableReward(address account) external view returns(uint256) {
+    function getClaimableReward(address account) external view returns (uint256) {
         uint256 currentClaimableReward = claimableReward[account];
         uint256 totalSupply = IPikaStaking(pikaStaking).totalSupply();
         if (totalSupply == 0) return currentClaimableReward;
 
         uint256 _pendingReward = IPikaPerp(pikaPerp).getPendingPikaReward() * PRECISION / tokenBase;
         uint256 _rewardPerTokenStored = cumulativeRewardPerTokenStored + _pendingReward * PRECISION / totalSupply;
-        if (_rewardPerTokenStored == 0) return currentClaimableReward;
         return currentClaimableReward + IPikaStaking(pikaStaking).balanceOf(account) * (_rewardPerTokenStored - previousRewardPerToken[account]) / PRECISION;
     }
 
-    fallback() external payable {}
-    receive() external payable {}
-
-    // Utils
-
     function _transferOut(address to, uint256 amount) internal {
-        if (amount == 0 || to == address(0)) return;
+        require(to != address(0), "Cannot transfer to zero address");
+        require(amount > 0, "Amount must be greater than zero");
         if (rewardToken == address(0)) {
             payable(to).sendValue(amount);
         } else {
             IERC20(rewardToken).safeTransfer(to, amount);
         }
-    }
-
-    modifier onlyOwner() {
-        require(msg.sender == owner, "!owner");
-        _;
     }
 }
